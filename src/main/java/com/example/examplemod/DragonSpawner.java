d3ec40815b39921fa47a72850e34ad0dbefaad4d
abdabTheCreator <abby.crimlis@outlook.com>
2023-05-08 05:43:32 +0000

    Initial commit

---
+++ src/main/java/com/example/examplemod/DragonSpawner.java
@@ -0,0 +1,24 @@
+package com.example.examplemod;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.boss.enderdragon.phases.EnderDragonPhase;
+import net.minecraft.world.level.block.Blocks;
+
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = "examplemod")
+public class DragonSpawner {
+    @SubscribeEvent
+    public static void spawnDragon(BlockEvent.EntityPlaceEvent event) {
+        if (event.getPlacedBlock().getBlock() == Blocks.DRAGON_EGG) {
+            event.getWorld().removeBlock(event.getPos(), false); // false = no flags
+            EnderDragon dragon = EntityType.ENDER_DRAGON.create(event.getEntity().getLevel());
+            dragon.moveTo(event.getPos(), 0, 0);
+            dragon.getPhaseManager().setPhase(EnderDragonPhase.TAKEOFF);
+            event.getWorld().addFreshEntity(dragon);
+        }
+    }
+}
